b264e965cd0dee1a78b70496594774453fb40168
AmmarAlkhooly98 <ammaralkhooly1@gmail.com>
2020-08-14 13:42:01 +0300

    connection socket init

--- client/src/components/Join.js
+++ client/src/components/Join.js
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Join = () => {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+
   return (
     <div>
       <h1>Join</h1>
