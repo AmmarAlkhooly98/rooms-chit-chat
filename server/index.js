8413167fdfd1e5bcd2ece17a5dd5f9b97ac1cc77
AmmarAlkhooly98 <ammaralkhooly1@gmail.com>
2020-08-15 00:36:04 +0300

    restructured front end

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ io.on("connection", (socket) => {
   console.log("We have a new connection!!!");
 
   socket.on("disconnect", () => {
-    console.log("User had left!!!");
+    console.log("User has left!!!");
   });
 });
 
